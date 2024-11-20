8f2497483bea291f07cf4280f72d3655c1934745
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-11-20 09:17:25 +0200

    for review and adjustments

--- finance-frontend/src/api.jsx
+++ finance-frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://databankvanguard-b3d326c04ab4.herokuapp.com/finance/',
   headers: {
     'Content-Type': 'multipart/form-data',
   },
